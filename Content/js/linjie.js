0048062b58bcf1cb69309e210f784af1d5eb164f
hihi_zh <444503829@qq.com>
2017-09-18 16:08:54 +0800

    修改了导航栏的载入时窗口的变化bug，添加了下滑按钮

---
+++ Content/js/linjie.js
@@ -0,0 +1,13 @@
+﻿
+//下箭头滑动
+$(document).ready(function () {
+    $("#a-down").click(function () {
+        var content_imgs_offset = $("#content-1").offset();
+        $("body,html").animate({
+            scrollTop: content_imgs_offset.top
+        }, 1500)
+    });
+
+});
+//标题特效
+new WOW().init();
