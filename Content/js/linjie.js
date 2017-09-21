bf3af3d824d40973f18227b0dca52251419580e2
hihi_zh <444503829@qq.com>
2017-09-21 19:40:48 +0800

    添加了采集用户信息的bootsharp模态框

--- Content/js/linjie.js
+++ Content/js/linjie.js
@@ -1,5 +1,36 @@
-﻿
-//下箭头滑动
+﻿//下箭头滑动
+var url = "Api//API//values/SaveUserData";
+jQuery.support.cors = true;
+
+//休眠函数
+function sleep(n) { //n表示的毫秒数
+    var start = new Date().getTime();
+    while (true) if (new Date().getTime() - start > n) break;
+}
+
+//提交函数
+function abc() {
+    let name = $("#name").val();
+    let phonenum = $("#phonenumber").val();
+    let email = $("#email").val();
+    let organization = $("#organization").val();
+    $.ajax({
+        type: "post",
+        url: url,
+        dataType: "json",
+        data:$('#form1').serialize(),
+        success: function (data) {
+            alert(data);
+
+            sleep(1000);
+
+            $("#close").trigger("click");
+        },
+        error: function () {
+            alert("发生未知错误，请重新提交")
+        }
+    });
+}
 $(document).ready(function () {
     $("#a-down").click(function () {
         var content_imgs_offset = $("#content-1").offset();
@@ -8,6 +39,6 @@ $(document).ready(function () {
         }, 1500)
     });
 
+    //产品的客户弹出框
 });
 //标题特效
-new WOW().init();
